1a82e935074b1d4c8148415aa5242179f44fb9f4
bcs <68373576@qq.com>
2018-09-03 11:15:05 +0800

    111

--- js/base/url.js
+++ js/base/url.js
@@ -1,6 +1,7 @@
 // const BASE_URL = 'http://127.0.0.1:5000/';
 
 var BASE_URL = 'http://gopig.io/slsAdminApi/';
+//var BASE_URL = 'https://oo600.com/slsAdminApi/';
 //var BASE_URL = 'http://dev.oo600.com/slsAdminApi/';
 var BASE_USER_URL = BASE_URL + 'user/'
 var USER_SEND_REGISTER_CODE_URL = BASE_USER_URL + 'sendRegisterCode'
